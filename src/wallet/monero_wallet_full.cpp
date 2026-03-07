d09e412a21a2d5671072d8dcc6619d4d4b95e411
everoddandeven <everoddandeven@protonmail.com>
2026-03-07 22:27:58 +0100

    Fix missing tx reference

--- src/wallet/monero_wallet_full.cpp
+++ src/wallet/monero_wallet_full.cpp
@@ -2291,6 +2291,7 @@ namespace monero {
       if (tx_blobs_iter != tx_blobs.end()) tx->m_full_hex = *tx_blobs_iter;
       if (tx_metadatas_iter != tx_metadatas.end()) tx->m_metadata = *tx_metadatas_iter;
       std::shared_ptr<monero_outgoing_transfer> out_transfer = std::make_shared<monero_outgoing_transfer>();
+      out_transfer->m_tx = tx;
       tx->m_outgoing_transfer = out_transfer;
       out_transfer->m_amount = *tx_amounts_iter;
       std::shared_ptr<monero_destination> destination = std::make_shared<monero_destination>(destinations[0]->m_address.get(), out_transfer->m_amount.get());
@@ -2427,6 +2428,7 @@ namespace monero {
       if (tx_blobs_iter != tx_blobs.end()) tx->m_full_hex = *tx_blobs_iter;
       if (tx_metadatas_iter != tx_metadatas.end()) tx->m_metadata = *tx_metadatas_iter;
       std::shared_ptr<monero_outgoing_transfer> out_transfer = std::make_shared<monero_outgoing_transfer>();
+      out_transfer->m_tx = tx;
       tx->m_outgoing_transfer = out_transfer;
       out_transfer->m_amount = *tx_amounts_iter;
 
@@ -2535,6 +2537,7 @@ namespace monero {
       if (tx_blobs_iter != tx_blobs.end()) tx->m_full_hex = *tx_blobs_iter;
       if (tx_metadatas_iter != tx_metadatas.end()) tx->m_metadata = *tx_metadatas_iter;
       std::shared_ptr<monero_outgoing_transfer> out_transfer = std::make_shared<monero_outgoing_transfer>();
+      out_transfer->m_tx = tx;
       tx->m_outgoing_transfer = out_transfer;
       out_transfer->m_amount = *tx_amounts_iter;
 
